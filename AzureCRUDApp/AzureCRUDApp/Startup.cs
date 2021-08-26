4df37120496c6101d3a6fdc0de81597ec263385c
JonathanCrafton <jhcrafton@gmail.com>
2021-08-26 15:53:14 -0500

    Initial Commit
    Link: https://azurecrudapp20210825033000.azurewebsites.net
    
    End User Login:
    Username: user@test.com
    Password: User#01
    
    Supervisor Login:
    Username: supervisor@test.com
    Password: Supervisor#01

---
+++ AzureCRUDApp/AzureCRUDApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureCRUDApp.Startup))]
+namespace AzureCRUDApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
